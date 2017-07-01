08847ac5f3538dce7f459e70efbb241d21e34ffd
Sanasol <mail@sanasol.ws>
2017-07-02 02:21:17 +0300

    analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,5 +69,21 @@
     <!-- Scripts -->
     <script async src="{{ mix('js/app.js') }}"></script>
 
+    @if(env('YAMETRIKA'))
+    <!-- Yandex.Metrika counter --> <script async type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter{{env('YAMETRIKA')}} = new Ya.Metrika({ id:{{env('YAMETRIKA')}}, clickmap:true, trackLinks:true, accurateTrackBounce:true, trackHash:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/{{env('YAMETRIKA')}}" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
+    @endif
+
+    @if(env('GA'))
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', '{{env('GA')}}', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+    @endif
 </body>
 </html>
