f46d611fd03e6954f2730997f357a8b434f81d59
Sanasol <mail@sanasol.ws>
2017-07-05 21:29:01 +0300

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app['request']->server->set('HTTPS', $this->app->environment() == 'production');
     }
 
     /**
