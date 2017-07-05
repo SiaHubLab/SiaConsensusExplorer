fdb34446341e14be16e8aafcceb6ebc0a1d4750d
Sanasol <mail@sanasol.ws>
2017-07-05 17:09:08 +0300

    cors

--- config/app.php
+++ config/app.php
@@ -178,6 +178,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
 
     ],
 
