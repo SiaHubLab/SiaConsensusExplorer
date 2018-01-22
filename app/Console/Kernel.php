756a02863906b911334d8e7b7d15e829a7fd4417
Sanasol <mail@sanasol.ws>
2018-01-23 02:15:51 +0700

    disable sitemap gen

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,8 +26,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('sitemap')
-                 ->hourly()->withoutOverlapping();
+//        $schedule->command('sitemap')
+//                 ->hourly()->withoutOverlapping();
 
         $schedule->command('blockmetrics')
                  ->everyTenMinutes()->withoutOverlapping();
